908bbd8b77492a1655a7e7b699b92859563034f9
YunduanLin <linyunduan0805@gmail.com>
2021-09-27 16:24:22 -0700

    add comments

--- agent.py
+++ agent.py
@@ -158,6 +158,12 @@ class Agv:
         return np.ceil(t_total), velocity_end, dist_move
 
     def find_feasible_interval(self, l_grid, cord):
+        '''
+        A function calculate the maximum straight-line distance of a starting point on both sides.
+        :arg l_grid (List[int]): layout of warehouse in one dimension.
+        :arg cord (int): the cordinate of starting point.
+        :return: cord_min, cord_max: the cordinate of points on both sides.
+        '''
         tmp = np.where(l_grid[:cord] != 0)[0]
         cord_min = (tmp[-1] + 1) if len(tmp) > 0 else 0
         tmp = np.where(l_grid[cord:] != 0)[0]
@@ -167,35 +173,40 @@ class Agv:
     def convert_to_ind(self, i, j):
         return i * self.width + j
 
-    # TODO: find the shortest path between two nodes while the velocity at origin/destination should be 0
-    #       1. avoid going through stations
-    #       2. find path
     def generate_shortest_path(self, grid):
+        '''
+        A function describes find the shortest path for each pair of grids.
+        :arg grid (Array): layout of warehouse, indicating where are the stations.
+        :return:
+        '''
         self.height, self.width = grid.shape
         self.cnt_grid = self.height * self.width
         dist = big_M * np.ones((self.height * self.width, self.height * self.width))
         next_grid = np.repeat([np.arange(0, self.cnt_grid, 1)], self.cnt_grid, axis=0)
         self.dist_straight = [self.move(0, dist)[0] for dist in range(max(self.height, self.width))]
 
-        # initialize straight line time for each pair
+        # initialize straight-line distance for each pair of grids
         for i in range(self.height):
             for j in range(self.width):
-                if grid[i, j] == 0:
+                if grid[i, j] == 0: # the grid is not a station
                     ind = self.convert_to_ind(i, j)
+                    # horizontal
                     j_min, j_max = self.find_feasible_interval(grid[i, :], j)
                     dist[ind, self.convert_to_ind(i,j_min):self.convert_to_ind(i,j + 1)] = self.dist_straight[:j - j_min + 1][::-1]
                     dist[ind, self.convert_to_ind(i,j):self.convert_to_ind(i,j_max + 1)] = self.dist_straight[:j_max - j + 1]
+                    # vertical
                     i_min, i_max = self.find_feasible_interval(grid[:, j], i)
                     dist[ind, self.convert_to_ind(i_min,j):self.convert_to_ind(i + 1,j):self.width] = self.dist_straight[:i - i_min + 1][::-1]
                     dist[ind, self.convert_to_ind(i,j):self.convert_to_ind(i_max + 1,j):self.width] = self.dist_straight[:i_max - i + 1]
                 else:
                     dist[i, i] = 0
 
+        # use Floyd algorithm to update the shortest distance and path
         for k in range(self.cnt_grid):
             for i in range(self.cnt_grid):
                 for j in range(self.cnt_grid):
                     if dist[i,k]+dist[k,j]+self.rotate_time < dist[i,j]:
-                        dist[i,j] = dist[i,k] + dist[k,j] + self.rotate_time
+                        dist[i,j] = dist[i,k] + dist[k,j] + self.rotate_time # add rotation time at each turning
                         next_grid[i,j] = k
 
         self.dist, self.next_grid = dist, next_grid
